84a76aeeecd1ea1896593eecb8e3a1f660739a75
Grupo-Gastronomia-CFP4 <grupogastrocfp4@gmail.com>
2026-08-31 08:35:16 -0300

    Add files via upload

--- funciones.js
+++ funciones.js
@@ -2,19 +2,23 @@ const imagenes = [
     "Multimedia/img2.jpg",
     "Multimedia/img3.jpg",
     "Multimedia/img9.jpg"
-]
+];
 
-let i = 0
-const imagen = document.getElementById("slider-imagenes")
+let i = 0;
+const imagen = document.getElementById("slider-imagenes");
 
 function cambiarImagen() {
-    i++
-    
-    if (i >= imagenes.length) {
-        i = 0
-    }
+    if (!imagen) return;
 
-    imagen.src = imagenes[i]
+    imagen.style.opacity = 0;
+
+    setTimeout(() => {
+        i = (i + 1) % imagenes.length;
+        imagen.src = imagenes[i];
+        imagen.style.opacity = 1;
+    }, 350);
 }
 
-setInterval(cambiarImagen, 5000)
+if (imagen) {
+    setInterval(cambiarImagen, 5000);
+}
